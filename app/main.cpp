aac1632ba400874d512af3bcee9a2e3069204b10
Michael <mvasilyev04@gmail.com>
2026-07-20 22:15:46 +0300

    Доделал многопоточность приложения

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,17 @@
+#include <condition_variable>
 #include <iostream>
 #include "Logger.hpp"
 #include <optional>
 #include <string>
 #include <sstream>
+#include <queue>
+#include <mutex>
+#include <thread>
+
+struct LogTask {
+    std::string text;
+    std::optional<LogLevel> level;
+};
 
 std::optional<LogLevel> parseLogLevel(std::string_view level) {
     if (level == "INFO") return LogLevel::Info;
@@ -26,12 +35,49 @@ int main(int argc, char** argv) {
     }
     Logger logger(filename, *level);
 
+    std::queue<LogTask> tasks;
+    std::mutex queueMutex;
+    std::condition_variable cv;
+    bool stop = false;
+    std::thread worker([&]() {
+        while(true) {
+            std::unique_lock<std::mutex> lock(queueMutex);
+
+            cv.wait(lock, [&]() {
+                    return !tasks.empty() || stop;
+            });
+
+            if (stop && tasks.empty()) {
+                break;
+            }
+
+            auto task = std::move(tasks.front());
+            tasks.pop();
+
+            lock.unlock();
+            try {
+                if (task.level.has_value()) {
+                    logger.log(task.text, *task.level);
+                } else {
+                    logger.log(task.text);
+                }
+            } catch(const std::exception& e) {
+                std::cerr << e.what() << '\n';
+            }
+        }
+    });
+
     std::string text;
     while(std::getline(std::cin, text)) {
         std::istringstream iss(text);
         std::string first;
         iss >> first;
         if (first == "/exit") {
+            {
+                std::lock_guard<std::mutex> lock(queueMutex);
+                stop = true;
+            }
+            cv.notify_one();
             break;
         }
         if (first == "/set") {
@@ -46,12 +92,22 @@ int main(int argc, char** argv) {
             continue;
         }
         auto level = parseLogLevel(first);
-        if (level.has_value()) {
-            std::string message;
-            std::getline(iss >> std::ws, message); 
-            logger.log(message, *level);
-        } else {
-            logger.log(text);
+        {
+            std::lock_guard<std::mutex> lock(queueMutex);
+            if (level.has_value()) {
+                std::string message;
+                std::getline(iss >> std::ws, message); 
+                if (message.empty()) {
+                    std::cerr << "Empty message\n";
+                    continue;
+                }
+                tasks.push(LogTask {message, level});
+            } else {
+                tasks.push(LogTask { text, std::nullopt});
+            }
         }
+        cv.notify_one();
     }
+    cv.notify_one();
+    worker.join();
 }
